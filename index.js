0db9732c2af70f7bae9ea4dffdc43913dd026687
John <johnkaranjairungu@gmail.com>
2023-03-19 18:27:56 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+multiply = (num1 * num2)
+num1=Math.random()*62;
+num2=Math.random()*62;
+num1*num2==62;
+if (num1*num2===62){
+    console.log(num1);
+    console.log (num2);
+}
 
 // Write your code here
